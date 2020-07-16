8382c18feec7d62cf8f51dc56e5c7e5e4f71fdae
Iris Rifold <irisrifold@gmail.com>
2020-07-16 13:35:17 +0300

    SASS

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
   <div id="app">
-    <nav>
-      <h1>Endorophin rush</h1>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/event">Event</router-link> |
-      <router-link to="/login">Login</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>  
-    <router-view/>
+    <header class="main-header">
+      <h1 class="logo">
+        Endorophin <span>r</span>ush
+      </h1>
+      <nav>
+        <router-link to="/">Home</router-link>|
+        <router-link to="/event">Event</router-link>|
+        <router-link to="/login">Login</router-link>|
+        <router-link to="/about">About</router-link>
+      </nav>
+    </header>
+    <router-view />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import eventFilter from '@/components/event-filter.cmp.vue'
+import eventFilter from "@/components/event-filter.cmp.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     eventFilter
   }
-}
+};
 </script>
 
 
