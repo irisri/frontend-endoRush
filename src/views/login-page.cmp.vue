0cbf01f345d7ea290ef9a45a761a5ceb3795b0f8
Lee <leedror33@gmail.com>
2020-07-16 12:59:22 +0300

    login

--- src/views/login-page.cmp.vue
+++ src/views/login-page.cmp.vue
@@ -4,21 +4,26 @@
       <user-profile />
     </div>
     <div v-else>
-      <h2>Login</h2>
-      <form class="flex column align-center" @submit.prevent="doLogin">
-        <input type="text" v-model="loginCred.userNmae" placeholder="Username" />
-        <input type="text" v-model="loginCred.password" placeholder="Password" />
-        <button>Login</button>
-      </form>
-      <h2>Signup</h2>
-      <form class="flex column align-center" @submit.prevent="doSignup">
-        <input type="text" v-model="signupCred.fullName" placeholder="Fullname" />
-        <input type="text" v-model="signupCred.userNmae" placeholder="Username" />
-        <input type="text" v-model="signupCred.password" placeholder="Password" />
-        <input type="file" @change="onUploadImg" />
-        <img :src="signupCred.imgUrl" />
-        <button>Signup</button>
-      </form>
+      <div v-if="!signUp">
+        <h2>Login</h2>
+        <form class="flex column align-center" @submit.prevent="doLogin">
+          <input type="text" v-model="loginCred.userNmae" placeholder="Username" />
+          <input type="text" v-model="loginCred.password" placeholder="Password" />
+          <button>Login</button>
+        </form>
+        <button @click="signUp=!signUp">Signup</button>
+      </div>
+      <div v-if="signUp">
+        <h2>Signup</h2>
+        <form class="flex column align-center" @submit.prevent="doSignup">
+          <input type="text" v-model="signupCred.fullName" placeholder="Fullname" />
+          <input type="text" v-model="signupCred.userNmae" placeholder="Username" />
+          <input type="text" v-model="signupCred.password" placeholder="Password" />
+          <input type="file" @change="onUploadImg" />
+          <img :src="signupCred.imgUrl" />
+          <button>Signup</button>
+        </form>
+      </div>
     </div>
   </section>
 </template>
@@ -29,12 +34,13 @@ export default {
   data() {
     return {
       loginCred: {},
-      signupCred: {}
+      signupCred: {},
+      signUp: false
     };
   },
   computed: {
     loggedInUser() {
-      return this.$store.getters.loggedInUser
+      return this.$store.getters.loggedInUser;
     }
   },
   methods: {
