7890eaa30d44ea19a620e4de68cc3d62b8287c8e
Ofra Bargad <Ofra.bargad@gmail.com>
2020-07-17 09:39:01 +0300

    changes in preview

--- src/components/evento-preview.cmp.vue
+++ src/components/evento-preview.cmp.vue
@@ -1,12 +1,25 @@
 <template>
-  <div class="evento-preview">
-       <img :src="evento.img">
-      <h3> {{ evento.title }} </h3>
-      <p> {{ evento.location.name }}
-          {{ evento.time }}
-          {{ evento.owner }}
-      </p>
-     
+  <div class="evento-preview flex-row">
+    <img style="width:200px;height:150px;" :src="img" />
+    <h3>{{ evento.title }}</h3>
+    <p>location: {{ evento.location.name }}</p>
+    <!-- <div>at: {{evento.starttime | relativeTime}}</div> -->
+    <div>at: {{timeToShow}}</div>
+    
+    <p>orgenised by {{ evento.owner }}</p>
+
+    <div class="rate" @click.stop="updateRate">
+      <input type="radio" id="star5" name="rate" value="5" />
+      <label for="star5" title="text">5 stars</label>
+      <input type="radio" id="star4" name="rate" value="4" />
+      <label for="star4" title="text">4 stars</label>
+      <input type="radio" id="star3" name="rate" value="3" />
+      <label for="star3" title="text">3 stars</label>
+      <input type="radio" id="star2" name="rate" value="2" />
+      <label for="star2" title="text">2 stars</label>
+      <input type="radio" id="star1" name="rate" value="1" />
+      <label for="star1" title="text">1 star</label>
+    </div>
   </div>
 </template>
 
@@ -20,5 +33,65 @@ export default {
       required: true
     }
   },
+  data() {
+    return {
+      img: this.evento.imgUrls[0]
+    };
+  },
+  computed: {
+     timeToShow() {
+    //   return new Date(this.evento.starttime.getTime());
+     return new Date(this.evento.starttime).toLocaleString()
+
+     }
+  },
+  methods: {
+    updateRate() {
+      console.log("rateunrate");
+    }
+  }
 };
 </script>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+.rate {
+  float: left;
+  height: 46px;
+  /* padding: 0 10px; */
+  margin-left: 30px;
+}
+.rate:not(:checked) > input {
+  position: absolute;
+  top: -9999px;
+}
+.rate:not(:checked) > label {
+  float: right;
+  width: 1em;
+  overflow: hidden;
+  white-space: nowrap;
+  cursor: pointer;
+  font-size: 30px;
+  color: #ccc;
+}
+.rate:not(:checked) > label:before {
+  content: "★ ";
+}
+.rate > input:checked ~ label {
+  color: #ffc700;
+}
+.rate:not(:checked) > label:hover,
+.rate:not(:checked) > label:hover ~ label {
+  color: #deb217;
+}
+.rate > input:checked + label:hover,
+.rate > input:checked + label:hover ~ label,
+.rate > input:checked ~ label:hover,
+.rate > input:checked ~ label:hover ~ label,
+.rate > label:hover ~ input:checked ~ label {
+  color: #c59b08;
+}
+</style>
