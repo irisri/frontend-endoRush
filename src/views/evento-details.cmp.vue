d518397965f0301b63c3d3bda7e0f71fd073b7eb
Lee <leedror33@gmail.com>
2020-07-17 15:18:13 +0300

    img details

--- src/views/evento-details.cmp.vue
+++ src/views/evento-details.cmp.vue
@@ -3,40 +3,33 @@
     <!-- <img v-if="eventoToShow.imgUrls[0]" :src="eventoToShow.imgUrls[0]"/> -->
     <!-- <img :src="http//res.cloudinary.com/do8gnvlal/image/upload/v1594639483/samples/landscapes/beach-boat.jpg"/> -->
     <h1>Toy Details:</h1>
-    <p>{{eventoToShow.imgUrls}}</p>
+    <div v-for="img in eventoToShow.imgUrls" :key="img">
+      <img :src="img" />
+    </div>
+    <p>{{eventoToShow.imgUrls[0]}}</p>
     <p>{{eventoToShow.title}}</p>
     <p>{{eventoToShow.members}}</p>
     <p>{{eventoToShow.location}}</p>
-    <!-- <p>{{eventoToShow.location.name}}</p> -->
+    <p>{{eventoToShow.location.name}}</p>
     <p>{{eventoToShow.starttime}}</p>
-    <!-- <div v-for="member in members" :key="evento.member.id">
+    <!-- <div v-for="member in eventoToShow.members" :key="member">
       <span>{{evento.member.id}}</span>
-      </div>  -->
+    </div> -->
   </div>
 </template>
 
 <script>
-import { eventoService } from "../services/eventoService.js";
-
 export default {
   data() {
     return {
       eventoToShow: null
     };
   },
-  methods: {
-    loadEvento() {
-      let { eventoId } = this.$route.params;
-      eventoService
-        .getById(eventoId)
-        .then(evento => (this.eventoToShow = evento));
-    }
-  },
   async created() {
     const eventoId = this.$route.params.id;
     await this.$store.dispatch({ type: "getById", eventoId });
     this.eventoToShow = this.$store.getters.evento;
-    console.log('this.eventoToShow', this.eventoToShow);
+    console.log("this.eventoToShow", this.eventoToShow);
   }
 };
 </script>
