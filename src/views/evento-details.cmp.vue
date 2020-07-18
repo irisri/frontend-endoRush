42efd2331c5a624bb8a40ce3a4b32ba26038f6bb
Lee <leedror33@gmail.com>
2020-07-18 11:55:32 +0300

    details

--- src/views/evento-details.cmp.vue
+++ src/views/evento-details.cmp.vue
@@ -1,19 +1,21 @@
 <template>
   <div v-if="eventoToShow" class="evento-details">
-    <!-- <img v-if="eventoToShow.imgUrls[0]" :src="eventoToShow.imgUrls[0]"/> -->
-    <!-- <img :src="http//res.cloudinary.com/do8gnvlal/image/upload/v1594639483/samples/landscapes/beach-boat.jpg"/> -->
-    <h1>Toy Details:</h1>
-    <div v-for="img in eventoToShow.imgUrls" :key="img">
-      <img :src="img" />
+    <div class="imgs-details" >
+      <img v-for="img in eventoToShow.imgUrls" :key="img" style="width:200px;height:150px;" :src="img" />
     </div>
-    <p>{{eventoToShow.title}}</p>
-    <p>{{eventoToShow.members}}</p>
-    <p>{{eventoToShow.location}}</p>
-    <p>{{eventoToShow.location.name}}</p>
-    <p>{{eventoToShow.starttime}}</p>
-    <!-- <div v-for="member in eventoToShow.members" :key="member">
-      <span>{{evento.member.id}}</span>
-    </div> -->
+    <h3>{{ eventoToShow.title }}</h3>
+    <div class="info">
+      <p>location: {{ eventoToShow.location.name }}</p>
+      <h4>at: {{timeToShow}}</h4>
+      <h4>orgenised by {{ eventoToShow.owner }}</h4>
+      <p>{{eventoToShow.title}}</p>
+      <p>{{eventoToShow.members}}</p>
+    </div>
+    <div class="members">
+    </div>
+    <div class="join">
+      <button @click="addMember()">Join</button>
+    </div>
   </div>
 </template>
 
@@ -24,11 +26,21 @@ export default {
       eventoToShow: null
     };
   },
+  computed: {
+    timeToShow() {
+      return new Date(this.eventoToShow.starttime).toLocaleString();
+    }
+  },
   async created() {
     const eventoId = this.$route.params.id;
     await this.$store.dispatch({ type: "getById", eventoId });
     this.eventoToShow = this.$store.getters.evento;
     console.log("this.eventoToShow", this.eventoToShow);
+  },
+  methods: {
+   addMember() {
+     this.$store.dispatch({ type: "addMember", evento });
+   }
   }
 };
 </script>
