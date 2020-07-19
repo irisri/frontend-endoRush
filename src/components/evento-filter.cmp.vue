2e965b4a60214b65b5aaf3d9859cf846812b2a34
Lee <leedror33@gmail.com>
2020-07-19 14:14:11 +0300

    move add evento

--- src/components/evento-filter.cmp.vue
+++ src/components/evento-filter.cmp.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="evento-filter">
-    <el-input size="large" @input="setFilter" v-model="filterBy.title" placeholder="find your next event"></el-input>
+    <el-input
+      size="large"
+      @input="setFilter"
+      v-model="filterBy.title"
+      placeholder="find your next event"
+    ></el-input>
     <el-select v-model="filterBy.time" @change="setFilter">
       <el-option v-for="item in dates" :key="item" :label="item" :value="item"></el-option>
     </el-select>
-    <el-select v-model="filterBy.tags" multiple @change="setFilter" placeholder="">
+    <el-select v-model="filterBy.tags" multiple @change="setFilter" placeholder>
       <el-option v-for="item in tags" :key="item" :label="item" :value="item"></el-option>
     </el-select>
+    <el-button title="Add New event" @click="$router.push('/evento/edit')">create new event!</el-button>
   </div>
 </template>
 
