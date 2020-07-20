238c117ce2bfdffb874df5aacacaf885e1c9ed58
Ofra Bargad <Ofra.bargad@gmail.com>
2020-07-20 19:19:01 +0300

    mm

--- src/views/user-details.cmp.vue
+++ src/views/user-details.cmp.vue
@@ -1,40 +1,41 @@
 
 <template>
-  <section class="user-details" v-if="userToShow">
-     <h1 >{{ userToShow.fullName }}</h1>
-      <img :src="userToShow.imgUrl" />
-      <h3>About... </h3>
-      <h4>{{ userToShow.bio }}</h4>
-       <h2>Reviews:</h2>
-        <ul>
-        <li v-for="review in reviews" :key="review">          
-            <h2>{{review.fullName}}</h2>
-            <el-rate v-model="review.rate" disabled></el-rate>
-            <h2>{{review.txt}}</h2>         
-        </li>
-        </ul>
+  <section v-if="userToShow" class="user-details" >
+    <h1>{{ userToShow.fullName }}</h1>
+    <img :src="userToShow.imgUrl" />
+    <h3>About...</h3>
+    <h4>{{ userToShow.bio }}</h4>
+    <h2>Reviews:</h2>
+    <!-- <ul>
+      <li v-for="(review, index) in reviews" :key="index">
+        <h2>{{review.fullName}}</h2>
+        <el-rate v-model="review.rate" disabled></el-rate>
+        <h2>{{review.txt}}</h2>
+      </li>
+    </ul> -->
   </section>
 </template>
 
 <script>
-
-
 export default {
   name: "user-details",
   data() {
     return {
-      userToShow: null,
-      reviews:[]
+      userToShow: '',
+      // reviews: []
     };
   },
   async created() {
     const userId = this.$route.params.id;
-    await this.$store.dispatch({ type: "getUserById", userId });
-    this.userToShow = this.$store.getters.user;
-    this.reviews = this.userToShow.reviews
-    console.log("details", userToShow);
-  },
-
+    if (userId) {
+      await this.$store.dispatch({ type: "getUserById", userId });
+      this.userToShow = this.$store.getters.user;
+      // this.reviews = this.userToShow.reviews;
+ 
+      return this.userToShow
+      
+    }
+  }
 };
 </script>
 
