115d22e5987198ca674c9c9142fdf8a29b5b92a2
Ofra Bargad <Ofra.bargad@gmail.com>
2020-07-20 13:17:40 +0300

    user page

--- src/router.js
+++ src/router.js
@@ -7,6 +7,7 @@ import eventoEdit from '@/views/evento-edit.cmp.vue'
 import loginPage from '@/views/login-page.cmp.vue'
 import aboutPage from '@/views/about-page.cmp.vue'
 import homePage from '@/views/home-page.cmp.vue'
+import userDetails from '@/views/user-details.cmp.vue'
 
 Vue.use(VueRouter)
 
@@ -40,7 +41,12 @@ const routes = [
     path: '/about',
     name: 'About',
     component: aboutPage
-  }
+  },
+  {
+    path: '/user/details/:id',
+    name: 'userDetails',
+    component: userDetails
+  },
 ]
 
 const router = new VueRouter({
