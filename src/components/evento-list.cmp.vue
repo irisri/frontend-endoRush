90ba206eda7214c40955a158b480e3528f871ec3
Ofra Bargad <Ofra.bargad@gmail.com>
2020-07-23 08:49:47 +0300

    list fix

--- src/components/evento-list.cmp.vue
+++ src/components/evento-list.cmp.vue
@@ -1,9 +1,5 @@
 <template>
   <div class="evento-list">
-<<<<<<< HEAD
-=======
-    <!-- <onlie-msg/> -->
->>>>>>> 3c0e2fc8e42d713d7e115e0596e29f1b882fe32b
       <section v-for="evento in eventos" :key="evento._id" @click.stop="$router.push(`/evento/details/${evento._id}`)">
         <evento-preview :evento="evento"/>
       </section>
