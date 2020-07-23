44fb1eaf4b1c8b0e81bad4d325ab9b3074b0908d
Ofra Bargad <Ofra.bargad@gmail.com>
2020-07-23 08:54:09 +0300

    header fix

--- src/components/evento-header.cmp.vue
+++ src/components/evento-header.cmp.vue
@@ -12,25 +12,16 @@
       <!-- <router-link to="/">Home</router-link>| -->
       <router-link to="/evento">Events</router-link>|
       <router-link to="/about">About</router-link>|
-<<<<<<< HEAD
-      <!-- <online-msg v-if="user"/> -->
-      <router-link v-if="user" v-bind:to="`/user/details/${user._id}`">Profile</router-link>
-=======
       <router-link v-if="user" :to="`/user/details/${user._id}`">Profile</router-link>
       <span v-if="user">|</span>
       <router-link v-if="user" @click.native="logout()" to="/">Logout</router-link>
->>>>>>> ba147ffe4acb96ac8e4590cd772e40b9e02eb323
       <router-link v-else to="/login">Login</router-link>
     </nav>
   </header>
 </template>
 
 <script>
-<<<<<<< HEAD
-import onlineMsg from "@/components/online-msg.cmp.vue"
-=======
 import SocketService from "@/services/SocketService";
->>>>>>> 3c0e2fc8e42d713d7e115e0596e29f1b882fe32b
 export default {
   props: {
     user: {
@@ -38,25 +29,15 @@ export default {
       required: false
     }
   },
-<<<<<<< HEAD
-<<<<<<< HEAD
-    components: {
-    onlineMsg
-=======
-=======
   data() {
     return {
       msg: {}
     };
   },
->>>>>>> 3c0e2fc8e42d713d7e115e0596e29f1b882fe32b
   methods: {
     logout() {
       this.$emit("logout");
     }
-<<<<<<< HEAD
->>>>>>> ba147ffe4acb96ac8e4590cd772e40b9e02eb323
-=======
   },
   async created() {
     SocketService.setup();
@@ -64,7 +45,6 @@ export default {
       this.msg = _msg;
     });
     console.log(this.msg);
->>>>>>> 3c0e2fc8e42d713d7e115e0596e29f1b882fe32b
   }
 };
 </script>
