5ab1b517ad0ba6bbb80036d41e361de07e7b02f4
Lee <leedror33@gmail.com>
2020-07-27 13:12:11 +0300

    tost

--- src/services/toastService.js
+++ src/services/toastService.js
@@ -7,7 +7,7 @@ function toastMsg(that, payload) {
   that.$toasted.show(payload.msg, {
     className: "alert-toast",
     icon: payload.icon,
-    duration: 5000,
+    duration: 15000,
     className: ["alert-toast"],
     position: "bottom-right",
   });
